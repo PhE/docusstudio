f766fe97cee139d9a25dd976cb45491dd68ea74b
Philippe Entzmann <philippe.entzmann@gmail.com>
2023-10-03 18:26:50 +0200

    fix: wip

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,12 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  docus: {
+    body: {
+      backgroundColor: {
+        initial: '#aadd88',
+        dark: '#0B0A0A'
+      }
+    }
+  }
 })
