764347feca1f207946137337632f8cc4abb5fe03
Philippe ENTZMANN <philippe@entzmann.name>
2023-10-05 15:31:38 +0200

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
-  extends: '@nuxt-themes/docus',
+  extends: '@arundo-tech/paxpar-widgets',
   devtools: { enabled: true },
 
   modules: [
