fe5ba96175530aa2ed94d2cf5b233ac4027b73b6
Philippe ENTZMANN <philippe@entzmann.name>
2023-10-05 16:08:54 +0200

    fix: wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
+import { createResolver, logger } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
+// That allows to overwrite these dependencies paths via `.env` for local development
+// fov local dev :
+// PAXPAR_WIDGETS_PATH=../paxpar-widgets
+const envModules = {
+  ppw: process?.env?.PAXPAR_WIDGETS_PATH || "@arundo-tech/paxpar-widgets",
+}
+
+
 export default defineNuxtConfig({
-  extends: '@arundo-tech/paxpar-widgets',
+  extends: [
+    envModules.ppw,
+  ],
   devtools: { enabled: true },
 
   modules: [
